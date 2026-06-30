378d98196fe90f7ba5da6ef925ebc23d5434e030
LuK1337 <priv.luk@gmail.com>
2026-06-30 13:51:29 -0400

    setup-makefiles: Make genrule inherit SoongModule
    Change-Id: I01ce21c8999f81b859a470d60f3108e27c8e86f3

--- setup-makefiles.py
+++ setup-makefiles.py
@@ -148,13 +148,12 @@ class dex_import(SoongPrebuilt):
         return self._to_blueprint(props)
 
 
-class genrule(SoongPrebuilt):
+class genrule(SoongModule):
     def __init__(self, filename: str, parts: list[str]):
-        self.filename = filename
-        self.install_path = "system/"
-        self.parts = parts
-        self.soong_module = "genrule"
+        super().__init__()
 
+        self.filename = filename
+        self.parts = parts
     def to_blueprint(self) -> str:
         props = {
             "name": f"{self.filename}-genrule",
